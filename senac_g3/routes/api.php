6e92a1de12f3daa616be999127310df3b3dcdf48
Gustavo Eidt <gustavo.eidt@gmail.com>
2022-05-12 19:05:01 -0300

    limpeza das rotas web

--- senac_g3/routes/api.php
+++ senac_g3/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::any('/teste', function () {
+    return response([1 => 3, 2, 3, 4])->header('Content-Type', 'application/json');
+});
