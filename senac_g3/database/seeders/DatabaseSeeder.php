1a13816fd0936a3ec5a20683bca18c74aae38e7a
Gustavo Eidt <gustavo.eidt@gmail.com>
2022-05-18 11:34:00 -0300

    seeder

--- senac_g3/database/seeders/DatabaseSeeder.php
+++ senac_g3/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProdutoSeeder;
+use Database\Seeders\ClienteSeeder;
+use Database\Seeders\PedidoSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            ProdutoSeeder::class,
+            ClienteSeeder::class,
+            PedidoSeeder::class,
+        ]);
     }
 }
