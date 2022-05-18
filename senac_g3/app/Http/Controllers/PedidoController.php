f3114f7c10494eaff7a2fad6012e1b734797fbe6
Gustavo Eidt <gustavo.eidt@gmail.com>
2022-05-18 11:33:34 -0300

    crud pedidos

---
+++ senac_g3/app/Http/Controllers/PedidoController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PedidoResource;
+use App\Models\Pedido;
+use Illuminate\Http\Request;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pedidos = Pedido::all();
+        return PedidoResource::collection($pedidos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $pedido = Pedido::create($request->all());
+        return new PedidoResource($pedido);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pedido $pedido)
+    {
+        return new PedidoResource($pedido);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pedido $pedido)
+    {
+        $pedido->update($request->all());
+        return new PedidoResource($pedido);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pedido $pedido)
+    {
+        $pedido->delete();
+        return response(null, 204);
+    }
+}
